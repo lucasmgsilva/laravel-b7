df4c0f18adab2f00087c90684ab3a8b6bc0d68f8
Lucas Matheus <lucasmgsilva@hotmail.com>
2021-08-21 14:33:50 -0300

    Alteração de configurações

--- config/database.php
+++ config/database.php
@@ -52,7 +52,7 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8',
+            'charset' => 'utf8mb3',
             'collation' => 'utf8_general_ci',
             'prefix' => '',
             'prefix_indexes' => true,
